70b2b99c6cf51f03bd5c801e0322b9ed6356c39f
jeremy.hon <jeremy.hon.gy@gmail.com>
2017-05-28 17:01:55 -0700

    try to do hot reloading and give up

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
-var path = require("path");
+const path = require("path");
+const webpack = require("webpack");
 process.traceDeprecation = true;
 module.exports = {
   context: __dirname + "/src",
-  entry: "./index.jsx",
+  entry: [
+    "react-hot-loader/patch",
+    // activate HMR for React
+
+    "webpack-dev-server/client?http://localhost:8080",
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    "webpack/hot/only-dev-server",
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    "./index.jsx"
+  ],
   output: {
+    filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    publicPath: "/assets/"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    hot: true,
+    inline: true,
+    port: 3001,
+    publicPath: "/assets/"
   },
   resolve: {
     extensions: [".js", ".jsx"]
@@ -59,5 +81,12 @@ module.exports = {
       // ** STOP ** Are you adding a new loader?
       // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+  ]
 };

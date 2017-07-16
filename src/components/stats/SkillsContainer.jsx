ff7989e7c741e2b4eac71215a5e888877e49f3f7
Jeremy Hon <jeremy.hon.gy@gmail.com>
2017-07-15 18:21:56 -0700

    add card sub list

--- src/components/stats/SkillsContainer.jsx
+++ src/components/stats/SkillsContainer.jsx
@@ -3,23 +3,35 @@ import PropTypes from 'prop-types'
 import _ from 'lodash'
 import classnames from 'classnames'
 
-import { skills } from 'lib/data'
+import CardSubList from 'components/card/CardSubList'
+import { skills as skillsToPick } from 'lib/data'
 import skillsPropType from './skillsPropType'
 import './StatsContainer.css'
 
-const renderSkills = (skills) => _.map(skills,
+// const renderSkills = (skills) => _.map(skills,
+//   (skill, key) => (
+//     <p key={key}>
+//       <span className="stat-name">{_.startCase(key)}:</span>
+//       <span className="stat-value">{skill}</span>
+//     </p>),
+// )
+
+const SkillsListRenderer = ({list}) => _.map(list,
   (skill, key) => (
     <p key={key}>
       <span className="stat-name">{_.startCase(key)}:</span>
       <span className="stat-value">{skill}</span>
-    </p>),
+    </p>
+  ),
 )
 
-export const SkillsContainer = (props) => (
-  <div className={classnames(props.className, 'stat-container')} >
-    <h3 className='stat-subtitle'>{props.subtitle}: </h3>
-    {renderSkills(_.pick(props.skills, skills))}
-  </div>
+export const SkillsContainer = ({subtitle, skills}) => (
+  <CardSubList
+    className="stat-container"
+    subtitle={subtitle}
+    list={_.pick(skills, skillsToPick)}
+    ListComponent={SkillsListRenderer}
+  />
 )
 
 SkillsContainer.defaultProps = {
